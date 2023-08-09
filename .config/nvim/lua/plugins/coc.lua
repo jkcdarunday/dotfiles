ef21900ad2ac66f0cfc6018ce7837b9da8a9d1fc
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-09 11:13:37 +0800

    Add global CoC extensions

---
+++ .config/nvim/lua/plugins/coc.lua
@@ -0,0 +1,19 @@
+vim.g.coc_global_extensions = {
+    "coc-snippets",
+    "coc-eslint",
+    "coc-emmet",
+    "coc-coverage",
+    "coc-solidity",
+    "coc-rust-analyzer",
+    "coc-lua",
+    "coc-json",
+    "coc-prettier",
+    "coc-tsserver",
+    "coc-html",
+    "coc-css",
+    "coc-yaml",
+    "coc-lists",
+    "coc-git",
+    "coc-highlight",
+    "coc-spell-checker"
+}
