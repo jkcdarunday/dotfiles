214b1e50d0dd5d4be173fcc2ad7c046faab46b21
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-10 21:32:43 +0800

    Fix autonightmode.lua by using kde-inhibit and waiting for mpv process to end

--- .config/mpv/scripts/autonightmode.lua
+++ .config/mpv/scripts/autonightmode.lua
@@ -1,25 +1,8 @@
 -- Author: Jan Keith Darunday <github@jkcdarunday.mozmail.com>
 -- Description: An MPV script for KDE users that inhibits night mode when you play a video
 
-local session_id
+local utils = require 'mp.utils'
+local mpv_pid = utils.getpid()
 
-function inhibit()
-    local session_id = tonumber(io.popen("qdbus org.kde.KWin /ColorCorrect org.kde.kwin.ColorCorrect.inhibit"):read("*all"))
-    print("Inhibited night mode with session ID " .. session_id)
-    return session_id
-end
+os.execute("kde-inhibit --colorCorrect tail --pid=" .. mpv_pid .. " -f /dev/null &")
 
-function uninhibit()
-    if (session_id ~= nil) then
-        io.popen("qdbus org.kde.KWin /ColorCorrect org.kde.kwin.ColorCorrect.uninhibit " .. session_id)
-        print("Uninhibited night mode with session ID " .. session_id)
-    end
-end
-
-mp.register_event("file-loaded", function()
-    session_id = inhibit()
-end)
-
-mp.register_event("shutdown", function()
-    uninhibit()
-end)
